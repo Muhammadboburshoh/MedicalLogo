e2590c29005fa314f6752c783a723fe522ae7d30
Muhammadboburshoh <muhammadbobur600@gmail.com>
2021-02-21 13:26:45 +0500

    Site header and intro part finished

--- src/Pages/HomePage/Intro/Intro.jsx
+++ src/Pages/HomePage/Intro/Intro.jsx
@@ -7,12 +7,13 @@ function Intro () {
       <div className="container">
         <div className="intro__right">
           <span className="intro__span">Get Best Insurance</span>
+          <span className="intro__circle"></span>
           <h1 className="intro__heading"><span> Welcome to</span><br/><p>Medical</p><br/>Insurance</h1>
           <p className="inpto__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae sit donec lectus suscipit ultricies rhoncus. Egestas platea in mauris urna gravida odio nam quisque </p>
           <h3 className="intro__bottom-heading">Which Type of Insurance you want?</h3>
 
-          <select name="insurance" id="select">
-            <option value="accident">Personal Accident Insurance</option>
+          <select className="intro__select" name="insurance" id="select">
+            <option className="intro__option" value="accident">Personal Accident Insurance</option>
           </select>
           <a href="#" className="green-link intro__link">Get a free Quote</a>
         </div>
