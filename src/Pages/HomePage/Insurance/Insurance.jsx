6d66b981caf674d7f37fdf94f926e8d87c2db718
Muhammadboburshoh <muhammadbobur600@gmail.com>
2021-04-14 17:02:14 +0500

    from css

--- src/Pages/HomePage/Insurance/Insurance.jsx
+++ src/Pages/HomePage/Insurance/Insurance.jsx
@@ -1,29 +1,45 @@
+import { Link } from "react-router-dom"
+
+import "./Insurance.css"
+
 function Insurance () {
 
   return (
     <section className="insurance">
-    <div className="container">
-    <form className="insurance__form">
-      <h3 className="insurance__form-header"></h3>
+      <div className="container">
+        <div className="insurance__wrapper">
+          <form className="insurance__form">
+            <h3 className="insurance__form-header">Get In Touch With Insurance Company!</h3>
 
-      <ul className="insurance__form-list">
-        <li className="insurance__form-item">
-          <input type="text" className="insurance__form-input"/>
-        </li>
-        <li className="insurance__form-item">
-          <input type="email" className="insurance__form-input"/>
-        </li>
-        <li className="insurance__form-item">
-          <input type="phone" className="insurance__form-input"/>
-        </li>
-        <li className="insurance__form-item">
-          <select className="insurance__form-select">
-            <option value="insurance">Insurance</option>
-          </select>
-        </li>
-      </ul>
-    </form>
-    </div>
+            <ul className="insurance__form-list">
+              <li className="insurance__form-item">
+                <input type="text" className="insurance__form-input" placeholder="Name*" />
+              </li>
+              <li className="insurance__form-item">
+                <input type="email" className="insurance__form-input" placeholder="Email*" />
+              </li>
+              <li className="insurance__form-item">
+                <input type="phone" className="insurance__form-input" placeholder="Phone" />
+              </li>
+              <li className="insurance__form-item">
+              <input type="phone" className="insurance__form-input" placeholder="Phone" />
+              </li>
+            </ul>
+            <button className="insurance__btn">Submit Now</button>
+          </form>
+        </div>
+      </div>
+
+      <div className="insurance__about">
+        <div className="container">
+          <div className="about__item-right insurance__about_wrapper">
+            <span className="about__item-header">Why Company Name</span>
+            <h3 className="about__item-heading">Why<br/> <span>Company Name?</span></h3>
+            <p className="about__item-text">Dhani App bring comprehensive Health care plan for all your necessary medical expenses for you powered by GoDigit Health Insurance. This insurance comes with cashless facility across the country in more than 4700+ hospitals. This insurance covers comes with number of features which includes COVID-19 & easy claim process.</p>
+            <Link to="#" className="about__btn">Know More</Link>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
